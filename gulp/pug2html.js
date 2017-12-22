f8827b98d3134ef61844b38978c49a3bff807c98
Zsolt Meszaros <zsolt@sowad.com>
2017-12-22 19:06:43 +0000

    Move meta variables to JSON

--- gulp/pug2html.js
+++ gulp/pug2html.js
@@ -1,10 +1,12 @@
 const gulp = require('gulp');
 const pug = require('gulp-pug');
+const locals = require('../src/data/locals.json');
 
 gulp.task('pugToHTML', () => (
   gulp.src('./src/pug/[^_]*.pug')
     .pipe(pug({
       pretty: true,
+      locals,
     }))
     .on('error', function onError(err) {
       console.error(err.toString());
